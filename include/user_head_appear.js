f12890dc79eca499894ccff5048e992f4c43add0
SmallSai <18023672372@163.com>
2020-03-27 09:48:49 +0800

    start

--- include/user_head_appear.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$("#user_cont").mouseleave(function(){
-		$("#user_operation_cont").fadeOut();
-	});
-	
-	$("#user_head_cont").mouseenter(function(){
-		$("#user_operation_cont").fadeIn();
-	});
-});
